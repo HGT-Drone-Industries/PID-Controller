763325feaa432d065400b45969daf97ea67c578e
N1cohd1 <125882726+N1cohd1@users.noreply.github.com>
2025-12-11 15:08:04 +0100

    followup to the former

--- Drone.java
+++ Drone.java
@@ -3,15 +3,17 @@
 // exec(open("C:/Users/KrutzlingerJakob/OneDrive - Hertzhaimer Gymnasium Trostberg/11a/P-Seminar Informatik/PID Controller Github Connection/PID-Controller/run.py").read())
 
 public class Drone {
-    static double xMovement;
-    static double zMovement;
-    static double setPoint;
-    public Drone(){
-        double xMovement = 0;
-        double zMovement = 0;
-        double setPoint = 0;
-    }
+    static double xRotation; //actual rotation
+    static double setPoint; //desired rotation
+    static int error; //difference between desired and actual rotation
+    static int looptime; //in ms
 
+    public Drone(){
+        xRotation = 0;
+        setPoint = 0;
+        error = 0; 
+        looptime = 1000;
+    }
 
     public static void main(String[] args) {
         System.out.println("Starting.");
@@ -19,10 +21,10 @@ public class Drone {
         System.out.println("Setpoint: " + setPoint);
     }
 
-
     public static double returnSetpoint(double newSetpoint){
         newSetpoint = Math.round(Math.random() * 1000);
-        //the following sets the range of the random number starting at 0, e. g 0 to 255 
+        //the following sets the range of the random number starting at 0, e. g 0 to 255
+        //at most 1000!
         while (newSetpoint > 255){
             newSetpoint = newSetpoint - 255;
         }
